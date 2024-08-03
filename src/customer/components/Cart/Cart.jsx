63e6119d87d80b8fccc1812d503619f25972d5ce
SANSKAR TUNDURWAR <sanskartundurwar2001@gmail.com>
2024-08-03 17:02:28 +0530

    Adding Cart and CartItem

--- src/customer/components/Cart/Cart.jsx
+++ src/customer/components/Cart/Cart.jsx
@@ -1,9 +1,54 @@
 import React from "react";
 import CartItem from "./CartItem";
+import { Button, Divider } from "@mui/material";
 const Cart = () => {
   return (
-    <div>
-      <CartItem />
+    <div className="">
+      <div className="lg:grid grid-cols-3 lg:px-16 relative">
+        <div className="col-span-2">
+          {[1, 1, 1, 1].map((item) => (
+            <CartItem />
+          ))}
+        </div>
+        <div className="px-5 sticky top-0 h-[110vh] mt-5 lg:mt-0">
+          <div className="border">
+            <p className="opacity-60 uppercase font-bold pt-4 pb-4">
+              Price Details
+            </p>
+            <hr />
+            <div>
+              <div className="space-y-3 font-semibold ">
+                <div className="flex justify-between p-3 text-black pb-0">
+                  <span>Price</span>
+                  <span>$ 3434</span>
+                </div>
+
+                <div className="flex justify-between p-3 text-black pb-0">
+                  <span>Discount</span>
+                  <span className="text-green-500">$ 3434</span>
+                </div>
+                <div className="flex justify-between p-3 text-black pb-0">
+                  <span>Delivery Charges</span>
+                  <span className="text-green-500">Free</span>
+                </div>
+                <div className="flex justify-between p-3 text-black pb-0">
+                  <span className="font-bold">Total Amount</span>
+                  <span className="text-green-500">$ 3434</span>
+                </div>
+              </div>
+              <div className="mt-10 ">
+                <Button
+                  className="w-full"
+                  variant="contained"
+                  sx={{ px: "2rem", py: "1rem", bgcolor: "#9155fd" }}
+                >
+                  Check Out
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/customer/components/Cart/CartItem.jsx
+++ src/customer/components/Cart/CartItem.jsx
@@ -1,5 +1,7 @@
+import { AddCircleOutline, RemoveCircleOutline } from "@mui/icons-material";
+import { Button, IconButton } from "@mui/material";
 import React from "react";
-
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 const CartItem = () => {
   return (
     <div className="p-5 border shadow-lg roundeed-md">
@@ -10,10 +12,30 @@ const CartItem = () => {
             src="https://rukminim1.flixcart.com/image/612/612/l5h2xe80/kurta/x/6/n/xl-kast-tile-green-majestic-man-original-imagg4z33hu4kzpv.jpeg?q=70"
           />
         </div>
-        <div className="ml-5 space-y-1">
+        <div className="ml-5 space-y-1 text-left">
           <p className="font-semibold">Men Slim kurta</p>
           <p className="opacity-70">Size, L White</p>
           <p className="opacity-70 mt-2">Seller:Christalio to fashion</p>
+          <div className="flex space-x-5 items-center  text-gray-900 mt-6 ">
+            <p className="font-semibold ">$1999</p>
+            <p className="opacity-50 line-through">$199</p>
+            <p className="text-green-300 font-semibold">90%</p>
+          </div>
+        </div>
+      </div>
+      <div className="lg:flex items-center lg:space-x-10 pt-4">
+        <div className="flex items-center space-x-2">
+          <IconButton>
+            <RemoveCircleOutline />
+          </IconButton>
+          <span className="py-1 px-7 border rounded-sm">3</span>
+          <IconButton sx={{ color: "RGB(145 85 253)" }}>
+            <AddCircleOutline />
+          </IconButton>
+        </div>
+
+        <div>
+          <Button>Remove</Button>
         </div>
       </div>
     </div>
